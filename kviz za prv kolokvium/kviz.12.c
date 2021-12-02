0b989f55058f488a3f11042c032ff67c27136313
Tamara <90916955+tamara-kostova@users.noreply.github.com>
2021-12-02 16:52:50 +0100

    Update kviz.12.c

--- kviz za prv kolokvium/kviz.12.c
+++ kviz za prv kolokvium/kviz.12.c
@@ -1,25 +1,30 @@
-/*Од тастатура се читаат непознат број на позитивни цели броеви (со максимум 5 цифри) се додека не се внесе нешто различно од
-број.За секој број треба да се најде позицијата (од десно на лево) на првото појавување на најголемата цифра во составот на
-бројот (цифрата на единици се наоѓа на позиција 0). Потоа, да се испечати статистика за позициите на најдените цифри со
-максимална вредност во следниот формат:
+/*Од тастатура се читаат непознат број на позитивни цели броеви (со максимум 5 цифри) се додека не се внесе нешто различно од број. За секој број треба да се најде позицијата
+(од десно на лево) на првото појавување на најголемата цифра во составот на бројот (цифрата на единици се наоѓа на позиција 0). Потоа, да се испечати статистика за позициите на 
+најдените цифри со максимална вредност во следниот формат:
+[позиција]: [вкупно броеви чија максимална цифра се наоѓа на таа позиција]
 */
 #include <stdio.h>
-int sumacifri(int n){
-    int suma=0;
-    while (n>0){suma+=n%10; n/=10;}
-    return suma;
+int pozicijanamaxcifra (int n){
+int max=0, p=0, i=0;
+while (n>0){
+    if (n%10>max) {max=n%10; p=i;}
+    n/=10; i++;
 }
-int maxcifra(int n){
-int max=0;
-while (n>0){if (n%10>max) max=n%10; n/=10;}
-return max;
+return p;
 }
-int main(){
-    int n, prethoden, segasen;
-    if (scanf("%d", &prethoden))
-        printf("%d: %d\n", prethoden, sumacifri(prethoden));
-    while (scanf("%d", &segasen)){
-        printf("%d: %d\n", segasen, maxcifra(prethoden)+sumacifri(segasen));
-        prethoden=segasen;}
+int main()
+{
+    int n, a=0, b=0, c=0, d=0, e=0;
+    while(scanf("%d", &n))
+        switch (pozicijanamaxcifra(n)){
+            case 0: e++; break; case 1: d++; break;
+            case 2: c++; break; case 3: b++; break;
+            case 4: a++; break;
+    }
+        printf("0: %d\n", e);
+        printf("1: %d\n", d);
+        printf("2: %d\n", c);
+        printf("3: %d\n", b);
+        printf("4: %d\n", a);
     return 0;
 }
