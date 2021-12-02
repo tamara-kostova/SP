3bf3026581a2b370a7fb211be7c5b2ff6d5b9443
Tamara <90916955+tamara-kostova@users.noreply.github.com>
2021-12-02 22:58:21 +0100

    Update lab4.6.c

--- lab 4/lab4.6.c
+++ lab 4/lab4.6.c
@@ -4,37 +4,28 @@
 Да се игнорираат фискалните сметки, коишто имаат сума на артиклите поголема од 30.000 денари и за истите да се испечати соодветна порака, како во тест примерите.*/
 #include <stdio.h>
 int main(){
-int m, n, maxrec=0;
-float maxtax=0;
+int a, c, m, n, maxrec=0, suma;
+float maxtax=0, ddv;
 scanf("%d", &m);
-int suma[m];
-float ddv[m];
-for (int j=0; j<m; j++){
-suma[j]=0;
-ddv[j]=0;
+for (int i=0; i<m;i++){
+suma=0, ddv=0;
 scanf("%d", &n);
-int a[n];
-char c[n];
 for (int i=0; i<n; i++){
-    scanf("%d %c",&a[i], &c[i]);
-    suma[j]+=a[i];
-    if (c[i]=='A')
-            ddv[j]+=a[i]*0.18;
-    if (c[i]=='B')
-            ddv[j]+=a[i]*0.05;
+    scanf("%d %c",&a, &c);
+    suma+=a;
+    if (c=='A') ddv+=a*0.18;
+    if (c=='B') ddv+=a*0.05;
+    printf("suma:%d\nddv:%f",suma, ddv);
 }
-}
-for (int j=0; j<m; j++){
-if (suma[j]<30000) {
-    printf("Total tax return is: %.2f\n", 0.15*ddv[j]);
-        if (suma[j]>maxrec)
-            maxrec=suma[j];
-        if (ddv[j]*0.15>maxtax){
-                maxtax=0.15*ddv[j];
-                maxrec=suma[j];}
+if (suma<=30000) {
+    printf("Total tax return is: %.2f\n", 0.15*ddv);
+        if (suma>maxrec) maxrec=suma;
+        if (ddv*0.15>maxtax){
+                maxtax=0.15*ddv;
+                maxrec=suma;}
 }
 else
-        printf("Sum %d is bigger than 30000\n", suma[j]);
+        printf("Sum %d is bigger than 30000\n", suma);
 }
 printf("Max amount of reciept: %d. Max tax return for reciepts: %.2f", maxrec, maxtax);
 return 0;
